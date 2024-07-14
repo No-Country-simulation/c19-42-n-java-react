f92b82f7dec18115f35221c48e2b7e347d329ae7
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-07-13 23:49:58 -0300

    feat: versión preliminar del header

--- src/app/shared/components/layouts/header/header.component.ts
+++ src/app/shared/components/layouts/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+	selector: 'app-header',
+	standalone: true,
+	imports: [],
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css', '/src/app/app.component.scss'],
 })
 export class HeaderComponent {
-
+	public isLogged = false;
 }
