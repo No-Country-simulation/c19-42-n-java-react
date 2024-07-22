eadb78d6b98a87eae7427b9b684fda2fe2b736bf
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-07-22 14:28:42 -0300

    feat: preparación de login para conexión con el back, agregado archivo proxy

--- src/app/pages/auth/login/services/userInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-	message?: string;
-	id: number;
-	firstName?: string;
-	lastName?: string;
-	userName?: string;
-	email?: string;
-}
--- src/app/shared/components/layouts/header/header.component.ts
+++ src/app/shared/components/layouts/header/header.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { LoginService } from '../../../../pages/auth/login/services/login.service';
-import { User } from '../../../../pages/auth/login/services/userInterface';
 
 @Component({
 	selector: 'app-header',
@@ -10,29 +8,8 @@ import { User } from '../../../../pages/auth/login/services/userInterface';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css', '/src/app/app.component.scss'],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-	userLoginOn: boolean = false;
-	userData?: User;
-	constructor(private loginService: LoginService) {}
-
-	ngOnInit(): void {
-		this.loginService.currentUserLoginOn.subscribe({
-			next: (userLoginOn) => {
-				this.userLoginOn = userLoginOn;
-			},
-		});
-
-		this.loginService.currentUserData.subscribe({
-			next: (userData) => {
-				this.userData = userData;
-			},
-		});
-	}
-
-	ngOnDestroy() {
-		this.loginService.currentUserLoginOn.unsubscribe();
-		this.loginService.currentUserData.unsubscribe();
-	}
+export class HeaderComponent {
+	constructor() {}
 
 	public headerRoutes = [
 		{
