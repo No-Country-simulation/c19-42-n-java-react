1961cd02927529ba67a864849f90045358c99bf8
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-07-24 17:55:13 -0300

    feat: arregladas rutas de fuentes, implementada nueva paleta de colores y versión terminada del header y footer

--- src/app/shared/components/layouts/header/header.component.ts
+++ src/app/shared/components/layouts/header/header.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { title } from 'process';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [RouterLink, RouterLinkActive],
+	imports: [RouterLink, RouterLinkActive, MatButtonModule],
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.css', '/src/app/app.component.scss'],
 })
 export class HeaderComponent {
 	constructor() {}
 
 	public headerRoutes = [
-		{ title: 'Inicio', link: '/' },
 		{
 			title: 'Cómo Adoptar',
 			link: '/how-to-adopt',
